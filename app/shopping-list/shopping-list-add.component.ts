3c788de5e706a598080f83717050dd7134ca1912
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-10-18 13:18:07 +0300

    Shopping list component

---
+++ app/shopping-list/shopping-list-add.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'rb-shopping-list-add',
+  templateUrl: 'shopping-list-add.component.html'
+})
+export class ShoppingListAddComponent {
+
+ }
---
+++ app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'rb-shopping-list',
+  templateUrl: 'shopping-list.component.html'
+})
+export class ShoppingListComponent {
+
+ }
