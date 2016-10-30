a28a9b8a8a26c5e14ecf836e820ecbcdf7299806
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-10-30 16:21:33 +0300

    Active class, delete, edit, routes

--- app/recipes/recipe-detail/recipe-detail.component.ts
+++ app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs/Rx';
 import { Recipe } from '../recipe';
 import { ShoppingListService } from '../../shopping-list/shopping-list.service';
@@ -15,7 +15,8 @@ export class RecipeDetailComponent implements OnInit, OnDestroy {
     selectedRecipe: Recipe;
     constructor(private sls: ShoppingListService, 
                 private route: ActivatedRoute,
-                private recipesService: RecipeService) {}
+                private recipesService: RecipeService,
+                private router: Router) {}
 
     ngOnInit(){
       this.subscription = this.route.params.subscribe(
@@ -24,6 +25,13 @@ export class RecipeDetailComponent implements OnInit, OnDestroy {
           this.selectedRecipe = this.recipesService.getRecipe(this.recipeIndex);
         }
       );
+    }
+    onEdit(){
+      this.router.navigate(['/recipes', this.recipeIndex, 'edit'])
+    }
+    onDelete(){
+      this.recipesService.deleteRecipe(this.selectedRecipe);
+      this.router.navigate(['/recipes']);
     }
     onAddToShoppingList() {
       this.sls.addItems(this.selectedRecipe.ingredients);
