fa75b7d7cab56a86a9e57963efab490db34853a9
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-11-26 08:40:42 +0300

    edit recipe form, fetching

--- app/recipes/recipe-edit/recipe-edit.component.ts
+++ app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,13 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
+
 import { Recipe } from '../recipe';
+import { RecipeService } from '../recipe.service';
+
 @Component({
   moduleId: module.id,
   selector: 'rb-recipe-edit',
   templateUrl: 'recipe-edit.component.html'
 })
-export class RecipeEditComponent implements OnInit{ 
-    constructor() {}
+export class RecipeEditComponent implements OnInit, OnDestroy { 
+  private recipeIndex: number;
+  private subscription: Subscription; 
+    constructor(private route: ActivatedRoute, 
+                private recipeservice: RecipeService ) {}
     ngOnInit(){
-        
+        let isNew = true;
+        this.subscription = this.route.params.subscribe(
+          (params: any) => { 
+          if (params.hasOwnProperty('id')) {
+            isNew = false;
+            this.recipeIndex = +params['id'];
+          } else {
+            isNew = true;
+          }
+          }
+        )
+    }
+    ngOnDestroy(){
+      this.subscription.unsubscribe();
     }
 }
