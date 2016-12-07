157a16ca0cf644f022967e23f1fd2086a779e6ab
Alexey Sidorovitch <sidorovitch@gmail.com>
2016-12-07 10:13:18 +0300

    Split modules

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,21 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { routing } from './app.routing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
+
 
 import { AppComponent }   from './app.component';
 import { HeaderComponent } from './header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingListAddComponent } from './shopping-list/shopping-list-add.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeStartComponent } from './recipes/recipe-start.component';
-
 import { DropdownDirective } from './dropdown.directive';
+import { HomeComponent } from './home.component';
 
 import { RecipeService } from './recipes/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
@@ -24,20 +19,14 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 @NgModule({
   imports:      [ BrowserModule, 
                   routing,
-                  FormsModule,
-                  ReactiveFormsModule,
-                  HttpModule ],
+                  HttpModule,
+                  ShoppingListModule,
+                  ],
   declarations: [ AppComponent, 
                   HeaderComponent, 
-                  RecipesComponent,
-                  RecipeListComponent,
-                  RecipeItemComponent,
-                  RecipeDetailComponent,
-                  ShoppingListComponent,
-                  ShoppingListAddComponent,
                   DropdownDirective,
-                  RecipeEditComponent,
-                  RecipeStartComponent ],
+                  HomeComponent
+                 ],
   bootstrap:    [ AppComponent ],
   providers:    [ RecipeService,
                   ShoppingListService,
---
+++ app/home.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    template: `
+        <h1>Hello to Recipe Book!</h1>
+    `
+})
+export class HomeComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
